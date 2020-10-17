9aefc37aae7b05fa324c70febc1088fa35adbb7b
Rohan Poreddy <poreddyrohan@gmail.com>
2020-10-17 22:13:23 +0000

    new added

--- bottle_1/new.py
+++ bottle_1/new.py
@@ -4,11 +4,10 @@ from bottle import default_app, route
 
 @route('/')
 def hello_world():
-<<<<<<< HEAD
-    return 'Hello from Bottle!'
-=======
-    return 'Changes done in Dev'
->>>>>>> 504d44db7694e45bf837b5fd5e646b529f26e21e
+
+    return 'new'
+
+
 
 application = default_app()
 
